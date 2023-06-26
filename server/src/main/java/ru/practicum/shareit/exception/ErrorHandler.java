7c6522f5c0a0df5c43a438f9fb7614b6c41c686d
LenzFF <113861256+LenzFF@users.noreply.github.com>
2023-06-26 21:05:37 +0000

    ТЗ-16 Итерация 2

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
